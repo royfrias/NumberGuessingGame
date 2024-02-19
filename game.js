5fc46e5e5e608767d36107063cd5ab8ad4465187
Your Name <130116701+royfrias@users.noreply.github.com>
2024-02-19 14:13:55 -0500

    second commit to my game

--- game.js
+++ game.js
@@ -24,44 +24,47 @@ async function guessNumber(){
   console.log("Let's play a game where I guess the Computer's number !")
   let secretNumber = await ask("Please enter a secret number: ");
   secretNumber = Number(secretNumber);
-  // console.log(`You entered: ${secretNumber}`);
 
   let min = 1;
-  let max = 100;
-  let guess; 
+  let max = 20;
   let numberOfTries = 1;
-  let answer = Math.floor(Math.random() * (max - min + 1)) + min;
-  answer = Number(answer)
-  console.log(typeof answer);
+  let guess = Math.floor(Math.random() * (max - min + 1)) + min;
+  let firstGuess = await ask(`Is this your number ${guess}? (Y/N)`);
+  firstGuess = Number(firstGuess);
+  // answer = Number(answer)
+  // console.log(typeof answer);
 
-
-  while (answer !== secretNumber) {
-    guess = Math.floor((min + max) / 2);
-    let feedback = await ask(`Is it ... ${guess}? (Y/N)`);
-
-
-    
-    if (feedback.toLowerCase() === "y") {
-      console.log(`Your number was ${guess}`);
-      break;
-    } else if (feedback.toLowerCase() === "n") {
-      let guide = await ask("Is it higher (H) or lower (L)? ");
-      if (guide.toLowerCase() === "h") {
-        min = guess + 1;
-      } else if (guide.toLowerCase() === "l") {
-        max = guess - 1;
+  
+  if(firstGuess !== secretNumber){
+    // let guide = await ask(`Is it higher (H) or lower (L)?`);
+    while (firstGuess !== secretNumber) {
+      guess = Math.floor((min + max) / 2);
+      let feedback = await ask(`Is it ... ${guess}? (Y/N)`);
+      // console.log(` CHECKING ${feedback}`);
+  
+      if (feedback.toLowerCase() === "y" || feedback.toLowerCase() === "Y" || feedback.toLowerCase() === "yes") {//guess was correct
+        break;
+      } else if (feedback.toLowerCase() === "n" || feedback.toLowerCase() === "no" || feedback.toLowerCase() === " " || feedback.toLowerCase() === "N") {
+        // guess was incorrect
+          let guide = await ask(`Is it higher (H) or lower (L)?`);
+          if (guide.toLowerCase() === "h") {
+            min = guess + 1;
+          } else if (guide.toLowerCase() === "l") {
+            max = guess - 1;
+          } else {
+            console.log(
+              "Invalid input, Please enter 'H' for higher or 'L' for lower."
+            );
+          }
       } else {
-        console.log("Please enter 'H' for higher or 'L' for lower.");
+        console.log("Invalid input, Please enter 'Y' for yes or 'N' for no.");
       }
-    } else {
-      console.log("Please enter 'Y' for yes or 'N' for no.");
+      numberOfTries++;
     }
-    numberOfTries++;
+    console.log(`Your number was ${guess}`);
+    console.log(`You win!! It took you ${numberOfTries} tries!`);
+    process.exit(); 
   }
-
-  console.log(`Number of tries: ${numberOfTries}`);
-  process.exit(); 
 }
-
 guessNumber();
 
